42d374f0d89c57a0684bbc26ec0fb191257f21fe
JessicaFresnedaH <jessyinad@gmail.com>
2016-11-24 11:20:34 -0500

    avance pdf

--- src/main/java/edu/eci/pdsw/samples/managedbeans/UsuarioBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/UsuarioBean.java
@@ -6,10 +6,10 @@
 package edu.eci.pdsw.samples.managedbeans;
 
 
+
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
-import edu.eci.pdsw.sample.bean.security.ShiroLoginBean;
 import edu.eci.pdsw.samples.entities.Usuario;
 import edu.eci.pdsw.samples.persistence.PersistenceException;
 import edu.eci.pdsw.samples.services.Servicios;
@@ -17,16 +17,9 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-import java.io.Serializable;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
-import javax.el.ELContext;
-import javax.faces.FacesException;
-import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import org.primefaces.model.DefaultStreamedContent;
@@ -47,8 +40,12 @@ public class UsuarioBean  {
     
     private String base="applicationconfig.properties";
 
-    @PostConstruct
-    public void init() {
+   
+
+    //==================================================================
+    public StreamedContent getStreamedContent() {
+        //1 sacar el usuaui
+        //construir texto
         try {
             //----------------------------------
             Document doc = new Document();
@@ -56,27 +53,26 @@ public class UsuarioBean  {
             OutputStream out = new ByteArrayOutputStream();
             PdfWriter writer = PdfWriter.getInstance(doc, out);
             doc.open();
-            doc.add(new Paragraph("Hdsfdjskfsdfs"));
-            doc.add(new Paragraph("Certificado AECI"));
+            doc.add(new Paragraph("Pdsw Grupo 3"));
+            
             doc.close();
             out.close();
 
             InputStream in = new ByteArrayInputStream(((ByteArrayOutputStream) out).toByteArray());
 
-            streamedContent = new DefaultStreamedContent(in, "application/pdf","c.pdf");
+            streamedContent = new DefaultStreamedContent(in, "application/pdf");
             //-------
             Map<String, Object> session = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
             byte[] b = (byte[]) session.get("reportBytes");
             if (b != null) {
-                streamedContent = new DefaultStreamedContent(new ByteArrayInputStream(b), "application/pdf", "c.pdf");
+                streamedContent = new DefaultStreamedContent(new ByteArrayInputStream(b), "application/pdf");
             }
         } catch (Exception e) {
         }
-
-    }
-
-    //==================================================================
-    public StreamedContent getStreamedContent() {
+        
+        
+        
+        
         if (FacesContext.getCurrentInstance().getRenderResponse()) {
             return new DefaultStreamedContent();
         } else {
@@ -89,22 +85,7 @@ public class UsuarioBean  {
         this.streamedContent = streamedContent;
     }
     
-   public static Object getManagedBean(final String beanName) {
-        FacesContext fc = FacesContext.getCurrentInstance();
 
-        Object bean=null;
-        try {
-            ELContext elContext = fc.getELContext();
-            bean = elContext.getELResolver().getValue(elContext, null, beanName);
-        } catch (RuntimeException e) {
-            throw new FacesException(e.getMessage(), e);
-        }
-
-        if (bean == null) {
-            throw new FacesException("Managed bean with name '" + beanName + "' was not found. Check your faces-config.xml or @ManagedBean annotation.");
-        }
-        return bean;
-    }
   
 
 

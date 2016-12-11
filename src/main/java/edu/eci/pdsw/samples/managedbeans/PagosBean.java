4fb0c08018e9fb0b86aec24d249cc1107e51892a
David Reina <miguel.rojas-ma@mail.escuelaing.edu.co>
2016-12-11 14:12:00 -0500

    Adelanto bean RegistroPago

--- src/main/java/edu/eci/pdsw/samples/managedbeans/PagosBean.java
+++ src/main/java/edu/eci/pdsw/samples/managedbeans/PagosBean.java
@@ -27,6 +27,11 @@ public class PagosBean implements Serializable{
     
     private Pago pagoselec;
     private String base="applicationconfig.properties";
+    private byte[] foto; 
+    private String IDpago;
+    private String TipoPago;
+    private String TipoTramite;
+    
     
     public List<Pago> getPagos() throws PersistenceException {
         List<Pago> a=Servicios.getInstance(base).consultarPago();
@@ -48,6 +53,35 @@ public class PagosBean implements Serializable{
         this.pagoselec = pagoselec;
     }
     
+    public void setFoto(byte[] foto){
+        this.foto = foto;
+    }
     
+    public byte[] getFoto(){
+        return this.foto;
+    } 
     
+    public void setIdPago(String id){
+        this.IDpago = id;
+    }
+    
+    public String getIdPago(){
+        return this.IDpago;
+    }
+    
+    public void setTipoPago(String tipo){
+        this.TipoPago = tipo;
+    }
+    
+    public String getTipoPago(){
+        return this.TipoPago;
+    }
+    
+    public void setTipoTramite(String tipo){
+        this.TipoTramite = tipo;
+    }
+    
+    public String getTipoTramite(){
+        return this.TipoTramite;
+    }
 }
